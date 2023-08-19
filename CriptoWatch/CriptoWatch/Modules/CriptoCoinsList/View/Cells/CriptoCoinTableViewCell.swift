e15cd321e0788d39c40ed118c4c0c213b7fa250e
Fernando Benavides <fernando@kinedu.com>
2023-08-19 13:40:01 +0200

    CryptoCoinTableViewCell view updated and reuseIdentifier static property added

--- CriptoWatch/CriptoWatch/Modules/CriptoCoinsList/View/Cells/CriptoCoinTableViewCell.swift
+++ CriptoWatch/CriptoWatch/Modules/CriptoCoinsList/View/Cells/CriptoCoinTableViewCell.swift
@@ -10,6 +10,11 @@ import UIKit
 
 final class CryptoCoinTableViewCell: UITableViewCell {
     
+    // MARK: - Static Properties
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+    
     // MARK: - Properties
     private let coinImageView: UIImageView = {
         let imageView = UIImageView()
@@ -61,7 +66,8 @@ final class CryptoCoinTableViewCell: UITableViewCell {
 // MARK: - Public API
 extension CryptoCoinTableViewCell {
     func configure(with coin: CriptoCoinRepresentable) {
-        coinImageView.image = UIImage(systemName: "bitcoinign.circle")
+        // TODO: - Replace with propiate image
+        coinImageView.image = UIImage(systemName: "bitcoinsign.circle.fill")
         nameLabel.text = coin.name
         currentPriceLabel.text = "Current Price: \(coin.currentPrice)"
         lastUpdatedLabel.text = "Last Updated: \(coin.lastUpdated)"
@@ -74,30 +80,45 @@ extension CryptoCoinTableViewCell {
 private extension CryptoCoinTableViewCell {
     
     func setupViews() {
+        
+        // Defining Stack Views
+        let nameAndPriceSV: UIStackView = UIStackView(arrangedSubviews: [nameLabel, currentPriceLabel])
+        nameAndPriceSV.spacing = 4
+        nameAndPriceSV.axis = .horizontal
+        nameAndPriceSV.distribution = .fillProportionally
+                                                           
+        let infoSV: UIStackView =  UIStackView(arrangedSubviews: [nameAndPriceSV, lastUpdatedLabel])
+        infoSV.spacing = 0
+        infoSV.axis = .vertical
+        infoSV.distribution = .fillEqually
+        infoSV.alignment = .leading
+        
+        let rootSV: UIStackView = UIStackView(arrangedSubviews: [coinImageView, infoSV, symbolLabel])
+        rootSV.spacing = 10
+        rootSV.axis = .horizontal
+        rootSV.translatesAutoresizingMaskIntoConstraints = false
+        rootSV.distribution = .fillProportionally
+        rootSV.alignment = .fill
+        
+        
+        NSLayoutConstraint.activate([
+            // Image
+            coinImageView.widthAnchor.constraint(equalToConstant: 50),
+            coinImageView.heightAnchor.constraint(equalToConstant: 50),
+            // Root Stack View
+            rootSV.heightAnchor.constraint(greaterThanOrEqualToConstant: 60),
+            rootSV.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            rootSV.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0),
+            rootSV.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            rootSV.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            
+        ])
+        
         contentView.addSubview(coinImageView)
         contentView.addSubview(nameLabel)
         contentView.addSubview(currentPriceLabel)
         contentView.addSubview(lastUpdatedLabel)
         contentView.addSubview(symbolLabel)
-        
-        NSLayoutConstraint.activate([
-            // Image
-            coinImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            coinImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            coinImageView.widthAnchor.constraint(equalToConstant: 40),
-            coinImageView.heightAnchor.constraint(equalToConstant: 40),
-            // Name
-            nameLabel.leadingAnchor.constraint(equalTo: coinImageView.trailingAnchor, constant: 16),
-            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12),
-            // Price
-            currentPriceLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            currentPriceLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 4),
-            // Las updated
-            lastUpdatedLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            lastUpdatedLabel.topAnchor.constraint(equalTo: currentPriceLabel.bottomAnchor, constant: 4),
-            // Symbol
-            symbolLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            symbolLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-        ])
+        contentView.addSubview(rootSV)
     }
 }
