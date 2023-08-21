1c324686307ddcab0c8a3d8fafaacd03c4000b6e
Fernando Benavides <fernando@kinedu.com>
2023-08-21 22:38:01 +0200

    CryptoDetailViewController addedCryptoDetailViewController added

---
+++ CriptoWatch/CriptoWatch/Modules/CryptoCoinDetail/Views/CryptoDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  CryptoDetailVC.swift
+//  CriptoWatch
+//
+//  Created by Fernando Benavides on 21/08/23.
+//
+
+import UIKit
+
+final class CryptoDetailVC: UIViewController {
+    
+    
+}
