523c26e2d2d4beb45c4f74d27deac365191b91e6
Fernando Benavides <fernando@kinedu.com>
2023-08-21 23:08:02 +0200

    Simple Image, price and symbol UI added toCryptoDetailVC

--- CriptoWatch/CriptoWatch/Modules/CryptoCoinDetail/Views/CryptoDetailVC.swift
+++ CriptoWatch/CriptoWatch/Modules/CryptoCoinDetail/Views/CryptoDetailVC.swift
@@ -9,5 +9,81 @@ import UIKit
 
 final class CryptoDetailVC: UIViewController {
     
+    // MARK: - UI Properties
+    private let image: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
+    private let currentPriceLabel: UILabel = {
+        let currentPriceLabel = UILabel()
+        currentPriceLabel.translatesAutoresizingMaskIntoConstraints = false
+        return currentPriceLabel
+    }()
+    
+    private let symbolLabel: UILabel = {
+        let symbolLabel = UILabel()
+        symbolLabel.translatesAutoresizingMaskIntoConstraints = false
+        return symbolLabel
+    }()
+    
+    private let totalVolume: UILabel = {
+        let totalVolume = UILabel()
+        totalVolume.translatesAutoresizingMaskIntoConstraints = false
+        return totalVolume
+    }()
+    
+    private let highestPrice24H: UILabel = {
+        let totalVolume = UILabel()
+        totalVolume.translatesAutoresizingMaskIntoConstraints = false
+        return totalVolume
+    }()
+    
+    private let lowestPrice24H: UILabel = {
+        let totalVolume = UILabel()
+        totalVolume.translatesAutoresizingMaskIntoConstraints = false
+        return totalVolume
+    }()
+    
+    private let priceChange24H: UILabel = {
+        let totalVolume = UILabel()
+        totalVolume.translatesAutoresizingMaskIntoConstraints = false
+        return totalVolume
+    }()
+    
+    private let marketCap: UILabel = {
+        let totalVolume = UILabel()
+        totalVolume.translatesAutoresizingMaskIntoConstraints = false
+        return totalVolume
+    }()
+    
+    
+}
+
+
+private extension CryptoDetailVC {
+    func setupView() {
+        self.view.addSubview(image)
+        self.view.addSubview(currentPriceLabel)
+        self.view.addSubview(symbolLabel)
+        
+        // Layout
+        NSLayoutConstraint.activate([
+            // Vertical Layout
+            image.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 20),
+            // Horizontal Layout
+            image.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            // Set Squar Aspect Ratio
+            image.widthAnchor.constraint(equalTo: image.heightAnchor, multiplier: 1.0),
+            
+            symbolLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            symbolLabel.topAnchor.constraint(equalTo: self.image.bottomAnchor, constant: 20),
+            symbolLabel.leadingAnchor.constraint(greaterThanOrEqualTo: self.view.leadingAnchor, constant: 20),
+            symbolLabel.trailingAnchor.constraint(greaterThanOrEqualTo: self.view.trailingAnchor, constant: 20),
+            
+            currentPriceLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            currentPriceLabel.topAnchor.constraint(equalTo: self.symbolLabel.bottomAnchor, constant: 20),
+        ])
+    }
 }
